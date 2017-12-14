758e943f16b2267d44f91cc795501a80344c5d94
cesarguerra98 <33233794+cesarguerra98@users.noreply.github.com>
2017-12-13 19:36:11 -0500

    ggg

--- mktypapp.py
+++ mktypapp.py
@@ -230,4 +230,4 @@ if __name__ == '__main__':
     app.run('0.0.0.0',port=5000)
 	#app.run('0.0.0.0',port=5300) # PRODUCTION gracehopper.cs-i.brandeis.edu:5300
     app.run('0.0.0.0',port=5000) # DEVELOPMENT 127.0.0.1:5000
-	app.run('0.0.0.0',port=5000) # DEVELOPMENT 127.0.0.1:5000
+    app.run('0.0.0.0',port=5000) # DEVELOPMENT 127.0.0.1:5000
