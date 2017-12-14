0f7f1ce080e1be890905b76952c50c077b4bf40f
cesarguerra98 <33233794+cesarguerra98@users.noreply.github.com>
2017-12-13 19:31:14 -0500

    changes

--- mktypapp.py
+++ mktypapp.py
@@ -229,4 +229,4 @@ def removeOrder():
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000)
 	#app.run('0.0.0.0',port=5300) # PRODUCTION gracehopper.cs-i.brandeis.edu:5300
-	app.run('0.0.0.0',port=5000) # DEVELOPMENT 127.0.0.1:5000
+    app.run('0.0.0.0',port=5000) # DEVELOPMENT 127.0.0.1:5000
